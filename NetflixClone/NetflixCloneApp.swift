84c9948aee0eea573a78239fc7d201a9da6dceb3
Ronald F. Paglinawan <ronaldpaglinawan@icloud.com>
2021-03-12 20:49:30 +0800

    initial commit

---
+++ NetflixClone/NetflixCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixCloneApp.swift
+//  NetflixClone
+//
+//  Created by Ronald Fornis Paglinawan on 12/03/21.
+//
+
+import SwiftUI
+
+@main
+struct NetflixCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
