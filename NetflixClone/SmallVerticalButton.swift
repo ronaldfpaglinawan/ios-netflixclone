785114a06f4b699b811b4ff20130db75e0ff3d9f
Ronald F. Paglinawan <ronaldpaglinawan@icloud.com>
2021-03-19 06:32:50 +0800

    added SmallVerticalButton.swift

---
+++ NetflixClone/SmallVerticalButton.swift
@@ -0,0 +1,65 @@
+//
+//  SmallVerticalButton.swift
+//  NetflixClone
+//
+//  Created by Ronald Fornis Paglinawan on 19/03/21.
+//
+
+import SwiftUI
+
+struct SmallVerticalButton: View {
+    var text: String
+    
+    var isOnImage: String
+    var isOffImage: String
+    
+    var isOn: Bool
+    
+    var imageName: String {
+        if isOn {
+            return isOnImage
+        } else {
+            return isOffImage
+        }
+    }
+    
+    
+    
+    
+    var action: () -> Void
+    
+    
+    var body: some View {
+        Button(action: {
+            action()
+        }, label: {
+            VStack {
+                Image(systemName: imageName)
+                    .foregroundColor(.white)
+                
+                Text(text)
+                    .foregroundColor(.white)
+                    .font(.system(size: 14))
+                    .bold()
+                
+            }
+        })
+    }
+}
+
+struct SmallVerticalButton_Previews: PreviewProvider {
+    static var previews: some View {
+        
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            
+            SmallVerticalButton(text: "My List",
+                                isOnImage: "checkmark",
+                                isOffImage: "plus",
+                                isOn: true) {
+                print("Tapped")
+            }
+        }
+    }
+}
