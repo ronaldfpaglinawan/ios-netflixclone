7062b54da40308deb75583e79f3fc757eeb8a149
Ronald F. Paglinawan <ronaldpaglinawan@icloud.com>
2021-03-18 15:45:39 +0800

    created the TopMoviePreview

---
+++ NetflixClone/TopMoviePreview.swift
@@ -0,0 +1,61 @@
+//
+//  TopMoviePreview.swift
+//  NetflixClone
+//
+//  Created by Ronald Fornis Paglinawan on 18/03/21.
+//
+
+import SwiftUI
+import Kingfisher
+struct TopMoviePreview: View {
+    var movie: Movie
+    
+    func isCategoryLast(_ cat: String) -> Bool {
+        let catCount = movie.categories.count
+        
+        if let index = movie.categories.firstIndex(of: cat) {
+            if index + 1 != catCount {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    var body: some View {
+        ZStack {
+            KFImage(movie.thumbnailURL)
+                .resizable()
+                .scaledToFill()
+                .clipped()
+            
+            VStack {
+                Spacer()
+                
+                HStack {
+                    ForEach(movie.categories, id: \.self) {
+                        category in
+                        HStack {
+                            Text(category)
+                            
+                            if !isCategoryLast(category) {
+                                Image(systemName: "circle.fill")
+                                    .foregroundColor(.green)
+                                    .font(.system(size: 3))
+                            }
+                        }
+                    }
+                }
+                
+                Text("Row of buttons")
+            }
+        }
+        .foregroundColor(.white)
+    }
+}
+
+struct TopMoviePreview_Previews: PreviewProvider {
+    static var previews: some View {
+        TopMoviePreview(movie: exampleMovie1)
+    }
+}
