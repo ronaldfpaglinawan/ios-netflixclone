59013a1b58a64b968a4897568933835297c58d68
Ronald F. Paglinawan <ronaldpaglinawan@icloud.com>
2021-03-25 06:08:17 +0800

    created Episode.swift and added MovieInfoSubheadline in MovieDetail

---
+++ NetflixClone/Model/Episode.swift
@@ -0,0 +1,22 @@
+//
+//  Episode.swift
+//  NetflixClone
+//
+//  Created by Ronald Fornis Paglinawan on 25/03/21.
+//
+
+import Foundation
+
+struct Episode: Identifiable {
+    var id = UUID().uuidString
+    
+    var name: String
+    var season: Int
+    var thumbnailImageURLString: String
+    var description: String
+    var length: Int
+    
+    var thumbnailURL: URL {
+        return URL(string: thumbnailImageURLString)!
+    }
+}
