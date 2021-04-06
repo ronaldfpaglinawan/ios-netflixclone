d29a597f0f90f9450899278c244c33270e881585
Ronald F. Paglinawan <ronaldpaglinawan@icloud.com>
2021-04-06 15:18:31 +0800

    integrated MovieDetailView to the HomeView

--- NetflixClone/HomeViews/HomeView.swift
+++ NetflixClone/HomeViews/HomeView.swift
@@ -12,6 +12,8 @@ struct HomeView: View {
     
     let screen = UIScreen.main.bounds
     
+    @State private var movieDetailToShow: Movie? = nil
+    
     var body: some View {
         ZStack {
             Color.black
@@ -43,12 +45,21 @@ struct HomeView: View {
                                         StandardHomeMovie(movie: movie)
                                             .frame(width: 100, height: 200)
                                             .padding(.horizontal, 20)
+                                            .onTapGesture(perform: {
+                                                movieDetailToShow = movie
+                                            })
                                     }
                                 }
                             }
                         }
                     }
                 }
+            }
+            
+            if movieDetailToShow != nil {
+                MovieDetail(movie: movieDetailToShow!, movieDetailToShow: $movieDetailToShow)
+                    .animation(.easeIn)
+                    .transition(.opacity)
             }
         }
         .foregroundColor(.white)
