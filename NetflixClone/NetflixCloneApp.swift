0dd9db544887ac6900d1f0b0678ea087fcb8a1dd
Ronald F. Paglinawan <ronaldpaglinawan@icloud.com>
2021-04-18 19:07:28 +0800

    added Cocopods to the project and added App icons

--- NetflixClone/NetflixCloneApp.swift
+++ NetflixClone/NetflixCloneApp.swift
@@ -6,9 +6,20 @@
 //
 
 import SwiftUI
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
 
 @main
 struct NetflixCloneApp: App {
+    
+    init() {
+        AppCenter.start(withAppSecret: "5ec63157-57d7-4179-959a-f5e87cb53e41", services:[
+          Analytics.self,
+          Crashes.self
+        ])
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
---
+++ Podfile
@@ -0,0 +1,6 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '13.0'
+
+target 'NetflixClone' do
+  pod 'AppCenter', '~> 4.1'
+end
