3b7df5fd9c7534ce3d2692d4ba40cf3a275f15c5
Ronald F. Paglinawan <ronaldpaglinawan@icloud.com>
2021-04-21 16:57:39 +0800

    created ActivityIndicator.swift and updated the SearchBar

--- NetflixClone/Search/SearchBar.swift
+++ NetflixClone/Search/SearchBar.swift
@@ -9,12 +9,14 @@ import SwiftUI
 
 struct SearchBar: View {
     
-    @State private var text: String = ""
+    @Binding var text: String
+    @State private var isEditing = true
+    @Binding var isLoading: Bool
     
     var body: some View {
         ZStack(alignment: .leading) {
             Color.graySearchBackground
-                .frame(width: 320, height: 36) // width: 270
+                .frame(width: 330, height: 36) // width: 270
                 .cornerRadius(8)
             
             HStack {
@@ -28,27 +30,45 @@ struct SearchBar: View {
                     .background(Color.graySearchBackground)
                     .cornerRadius(8)
                     .foregroundColor(.white)
-                    
+                    .onTapGesture(perform: {
+                        isEditing = true
+                    })
                 
-                Button(action: {
-                    // Clear text
-                    
-                }, label: {
-                    Image(systemName: "xmark.circle.fill")
-                        .foregroundColor(.graySearchText)
+                if !text.isEmpty {
+                    if isLoading {
+                        Button(action: {
+                            text = ""
+                        }, label: {
+                            ActivityIndicator(style: .medium, animate: .constant(true))
+                                .configure {
+                                    $0.color = .white
+                                }
+                        })
+                        .padding(.trailing, 32)
                         .frame(width: 35, height: 35)
-                })
-                .padding(.trailing, 18)
+                    } else {
+                        Button(action: {
+                            text = ""
+                        }, label: {
+                            Image(systemName: "xmark.circle.fill")
+                                .foregroundColor(.graySearchText)
+                                .frame(width: 35, height: 35)
+                        })
+                        .padding(.trailing, 18)
+                    }
+                }
                 
-                
-                Button(action: {
-                    // Clear text, hide both buttons, give up first-responder
-                }, label: {
-                    Text("Cancel")
-                        .foregroundColor(.white)
-                })
-                .padding(.trailing, 10)
-                
+                if isEditing {
+                    Button(action: {
+                        text = ""
+                        isEditing = false
+                        hideKeyboard()
+                    }, label: {
+                        Text("Cancel")
+                            .foregroundColor(.white)
+                    })
+                    .padding(.trailing, 10)
+                }
             }
         }
     }
@@ -60,7 +80,7 @@ struct SearchBar_Previews: PreviewProvider {
             Color.black
                 .edgesIgnoringSafeArea(.all)
             
-            SearchBar()
+            SearchBar(text: .constant(""), isLoading: .constant(false))
                 .padding()
         }
     }
