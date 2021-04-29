bc2debf3875a9163a8a2a944c3d3ec8ebdf62926
Ronald F. Paglinawan <ronaldpaglinawan@icloud.com>
2021-04-29 12:00:32 +0800

    added the Popular Search functionality in SearchView

---
+++ NetflixClone/Search/PopularMovieView.swift
@@ -0,0 +1,53 @@
+//
+//  PopularMovieView.swift
+//  NetflixClone
+//
+//  Created by Ronald Fornis Paglinawan on 29/04/21.
+//
+
+import SwiftUI
+import Kingfisher
+
+struct PopularMovieView: View {
+    var movie: Movie
+    
+    @Binding var movieDetailToShow: Movie?
+    
+    var body: some View {
+        GeometryReader { proxy in
+            HStack {
+                KFImage(movie.thumbnailURL)
+                    .resizable()
+                    .frame(width: proxy.size.width / 3)
+                    .padding(.leading, 3)
+                Text(movie.name)
+                
+                Spacer()
+                
+                Button(action: {
+                    // play movie
+                }, label: {
+                    Image(systemName: "arrowtriangle.right.fill")
+                })
+                .padding(.trailing, 20)
+            }
+            .foregroundColor(.white)
+            .onTapGesture(perform: {
+                self.movieDetailToShow = movie 
+            })
+        }
+    }
+}
+
+struct PopularMovieView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            
+            PopularMovieView(movie: exampleMovie1, movieDetailToShow: .constant(nil))
+                .frame(height: 75)
+                .padding(.horizontal, 50)
+        }
+    }
+}
