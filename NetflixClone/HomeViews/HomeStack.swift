feb6b93be04d202c7ea78e9dd0c3d6540d87f840
Ronald F. Paglinawan <ronaldpaglinawan@icloud.com>
2021-05-10 11:55:18 +0800

    created the DownloadsView and updated the HomeStack

--- NetflixClone/HomeViews/HomeStack.swift
+++ NetflixClone/HomeViews/HomeStack.swift
@@ -30,8 +30,7 @@ struct HomeStack: View {
                     LazyHStack {
                         ForEach(vm.getMovie(forCat: category, andHomeRow: topRowSelection, andGenre: selectedGenre)) { movie in
                             StandardHomeMovie(movie: movie)
-                                .frame(width: 100, height: 200)
-                                .padding(.horizontal, 20)
+                                .frame(width: 135, height: 200)
                                 .onTapGesture(perform: {
                                     movieDetailToShow = movie
                                 })
@@ -39,6 +38,7 @@ struct HomeStack: View {
                     }
                 }
             }
+            .padding(.leading, 6)
         }
     }
 }
